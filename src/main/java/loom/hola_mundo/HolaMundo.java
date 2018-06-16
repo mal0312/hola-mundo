a9740b765ff1d66dc6ece19a8655a56d82cdd602
Unknown <javi.deracing18@gmail.com>
2018-06-16 00:40:59 -0300

    utf8
    correccion

--- src/main/java/loom/hola_mundo/HolaMundo.java
+++ src/main/java/loom/hola_mundo/HolaMundo.java
@@ -6,6 +6,6 @@ package loom.hola_mundo;
 public class HolaMundo {
 
     public String saludar(final String persona) {
-    	 return "¡Hola, " + persona + "!";
+    	 return "Â¡Hola, " + persona + "!";
     }
 }
